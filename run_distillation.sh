6144aabb651d7db89f3ce1ce5cd7352664593b39
trishankusarma <trishankusarma165@gmail.com>
2026-04-27 14:52:51 +0530

    done with full pipeline

--- run_distillation.sh
+++ run_distillation.sh
@@ -18,41 +18,41 @@ sleep 3
 export CUDA_VISIBLE_DEVICES=0
 export VLLM_MEMORY_PROFILER_ESTIMATE_CUDAGRAPHS=1
 
-# python dataset_generation.py \
-#     --teacher_model Qwen/Qwen2.5-7B-Instruct \
-#     --num_samples 3500,2500,2000,1000,1000 \
-#     --output_file outputs/train.jsonl \
-#     --max_new_tokens 512 \
-#     --gpu_memory_utilization 0.85 \
-#     --tensor_parallel_size 1 \
-#     --batch_size 10000
+python dataset_generation.py \
+    --teacher_model Qwen/Qwen2.5-7B-Instruct \
+    --num_samples 3500,2500,2000,1000,1000 \
+    --output_file outputs/train.jsonl \
+    --max_new_tokens 512 \
+    --gpu_memory_utilization 0.85 \
+    --tensor_parallel_size 1 \
+    --batch_size 10000
 
-# # In-family student (Qwen)
-# python train_distill.py \
-#     --student_model Qwen/Qwen2.5-1.5B-Instruct \
-#     --train_data outputs/train.jsonl \
-#     --output_dir outputs/qwen_distilled \
-#     --log_file_name qwen_distill \
-#     --epochs 3 \
-#     --early_stopping_patience 2 \
-#     --val_max_new_tokens 512 \
-#     --batch_size 2 \
-#     --grad_accumulation_steps 4 \
-#     --val_acc_samples_per_lang 10 \
-#     --max_length  1702
+# In-family student (Qwen)
+python train_distill.py \
+    --student_model Qwen/Qwen2.5-1.5B-Instruct \
+    --train_data outputs/train.jsonl \
+    --output_dir outputs/qwen_distilled \
+    --log_file_name qwen_distill \
+    --epochs 3 \
+    --early_stopping_patience 2 \
+    --val_max_new_tokens 512 \
+    --batch_size 2 \
+    --grad_accumulation_steps 4 \
+    --val_acc_samples_per_lang 10 \
+    --max_length  1702
 
-# python train_distill.py \
-#     --student_model "meta-llama/Llama-3.2-1B-Instruct"\
-#     --train_data outputs/train.jsonl \
-#     --output_dir outputs/llama_distilled \
-#     --log_file_name llama_distill \
-#     --epochs 3 \
-#     --early_stopping_patience 2 \
-#     --val_max_new_tokens 512 \
-#     --batch_size 2 \
-#     --grad_accumulation_steps 4 \
-#     --val_acc_samples_per_lang 10 \
-#     --max_length  1702
+python train_distill.py \
+    --student_model "meta-llama/Llama-3.2-1B-Instruct"\
+    --train_data outputs/train.jsonl \
+    --output_dir outputs/llama_distilled \
+    --log_file_name llama_distill \
+    --epochs 3 \
+    --early_stopping_patience 2 \
+    --val_max_new_tokens 512 \
+    --batch_size 2 \
+    --grad_accumulation_steps 4 \
+    --val_acc_samples_per_lang 10 \
+    --max_length  1702
 
 python inference_eval.py \
     --base_model Qwen/Qwen2.5-1.5B-Instruct \
@@ -62,9 +62,9 @@ python inference_eval.py \
     --report_file outputs/metrics_qwen.txt
 
 # Llama distilled
-# python inference_eval.py \
-#     --base_model "meta-llama/Llama-3.2-1B-Instruct"\
-#     --adapter_path outputs/llama_distilled/best \
-#     --test_data outputs/test.jsonl \
-#     --output_predictions outputs/predictions_llama.jsonl \
-#     --report_file outputs/metrics_llama.txt
+python inference_eval.py \
+    --base_model "meta-llama/Llama-3.2-1B-Instruct"\
+    --adapter_path outputs/llama_distilled/best \
+    --test_data outputs/test.jsonl \
+    --output_predictions outputs/predictions_llama.jsonl \
+    --report_file outputs/metrics_llama.txt
